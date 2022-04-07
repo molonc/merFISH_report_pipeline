6ae0508ce443e5c2c7be5508298ee2b44ddd2acb
Shahid Haider <shaider@bccrc.ca>
2022-04-07 09:16:22 -0700

    Extended Base Report to accomodate lists of image stacks and to be able to merge lists of coord info files

--- reports/BaseReport.py
+++ reports/BaseReport.py
@@ -2,11 +2,40 @@ import json
 import numpy as np
 class BaseReport:
     # Base class for all the reports
-    def __init__(self,imgstack_file,coord_info):
+    def __init__(self,imgstack_files,coord_infos):
         
-        self.imgstack = np.load(imgstack_file)
-        a_file = open(coord_info, "r")
-        self.coords = json.load(a_file)
+        if isinstance(imgstack_files,list):
+            #If there is a list of images, then extend the image stack along the last dimension
+            _imgstack = np.array([np.load(image_stack) for image_stack in imgstack_files]) 
+            self.imgstack = np.stack(_imgstack,axis = len(_imgstack.shape))
+        else:
+            self.imgstack = np.load(imgstack_files)
+
+        if isinstance(coord_infos,list):
+            merged_json = dict()
+            for coord_info in coord_infos:
+                a_file = open(coord_info, "r")
+                _contents = json.load(a_file)
+
+                for k,v in _contents.items():
+                    if not (k in merged_json):
+                        #if the key isnt in the merged dictionary, then add and initialise it. This is python 3.6+ behaviour
+                        merged_json[k] = v
+                        continue
+                    
+                    if len(set(merged_json[k],v))==len(set(merged_json[k])):
+                        # If there is nothing unique when making the set of the new content at that key and the old content, then skip
+                        continue
+
+                    if not isinstance(merged_json[k],list):
+                        # If there is unique data and the value at k in merged dict is not a list already, make it one
+                        merged_json[k] = [merged_json[k]]
+                    
+                    merged_json[k].extend(v) #We use extend, not append here, in case the object is another iterable. We want to keep the list flat. 
+            self.coords = merged_json
+        else:
+            a_file = open(coord_infos, "r")
+            self.coords = json.load(a_file)
 
         self.pdf = None
     #PDF controls--------------------------------------------
